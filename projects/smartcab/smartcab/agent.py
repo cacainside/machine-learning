268bf295458bcab49213d504334e404ce5b02ec2
cacainside <cacainside@gmail.com>
2019-01-15 23:16:31 +0800

    change alpha to 0.75

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -196,7 +196,7 @@ def run():
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
     #agent = env.create_agent(LearningAgent, learning=True, alpha=(math.pi/20), epsilon=1)
-    agent = env.create_agent(LearningAgent, learning=True, alpha=0.2, epsilon=1)
+    agent = env.create_agent(LearningAgent, learning=True, alpha=0.75, epsilon=1)
     ##############
     # Follow the driving agent
     # Flags:
@@ -210,14 +210,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, display=True, update_delay=0.01, log_metrics=True, optimized=True)
+    sim = Simulator(env, display=False, update_delay=0.001, log_metrics=True, optimized=True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=50, tolerance=0.05)
+    sim.run(n_test=10, tolerance=0.05)
 
 
 if __name__ == '__main__':
